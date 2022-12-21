b3cc3c227e453d12d2524dd8a5186ca4cdca6267
Alena Deviatova <aedeviatova@yandex.ru>
2022-12-21 15:34:01 +0300

    feat: save store in localStorage

--- src/components/store/store.ts
+++ src/components/store/store.ts
@@ -1,11 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cart from './reducers/cart';
+import {saveState} from "./reducers/storeDb";
 
 /**
  * инициализируем и возвращаем redux store. устанавливаем нужные редюсеры.
  */
-export default configureStore({
+const store = configureStore({
   reducer: {
     cart: cart,
   },
 });
+
+saveState(store)
+
+export default store
