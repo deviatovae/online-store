bd11dfee78f06869ec9a8a4a4cfef739a3bfb36a
AlexeiKozlovskiy <gekleberefin@gmail.com>
2022-12-31 00:31:36 +0300

    feat:add validation

--- src/components/view/pages/payment/paymentPageView.ts
+++ src/components/view/pages/payment/paymentPageView.ts
@@ -5,6 +5,11 @@ import {CartDataType} from "../../../types/cartDataType";
 import {formatPrice} from "../../helpers/helpers";
 import {HeaderView} from "../../header/headerView";
 import {FooterView} from "../../footer/footerView";
+import {Controller} from "../../../controller/controller"; 
+
+// <div class="cards__img cards__img_mastercard"></div>
+// <div class="cards__img cards__img_amex"></div>
+
 
 export class PaymentPageView extends View<CartDataType> {
     protected views = {
@@ -24,27 +29,25 @@ export class PaymentPageView extends View<CartDataType> {
                   <div class="payment-details__credentials">
                     <div class="payment-details__title">PAYMENT DETAILS</div>
                     <div class="payment-details__info">
-                      <input type="text" class="payment-details__name" placeholder="Name">
-                      <input type="text" class="payment-details__shipping-address" placeholder="Address">
-                      <input type="text" class="payment-details__email" placeholder="Email">
-                      <input type="text" class="payment-details__phone-number" placeholder="+7">
+                      <input type="text" class="payment-details__name" required="required" placeholder="Name">
+                      <input type="text" class="payment-details__shipping-address" required="required" placeholder="Address">
+                      <input type="email" class="payment-details__email" required="required" placeholder="Email">
+                      <input type="tel" class="payment-details__phone-number" required="required"  placeholder="Phone +7 ...">
                     </div>
                   </div>
                   <div class="payment-details__payment-method payment-method">
                     <div class="payment-method__top">
                       <div class="payment-method__title">PAYMENT METHOD</div>
                       <div class="payment-method__cards cards">
-                        <div class="cards__img cards__img_visa"></div>
-                        <div class="cards__img cards__img_mastercard"></div>
-                        <div class="cards__img cards__img_amex"></div>
+                        <div class="cards__img"></div>
                       </div>
                     </div>
                     <div class="payment-method__card-details card-details">
-                      <input type="text" class="card-details__name" placeholder="Name on card">
-                      <input type="text" class="card-details__card-number" placeholder="5xxx xxxx xxxx xxxx">
+                      <input type="text" class="card-details__name" required="required" placeholder="Name on card">
+                      <input type="text" class="card-details__card-number" required="required" placeholder="хxxx xxxx xxxx xxxx">
                       <div class="card-details__bottom-row bottom-row">
-                        <input type="text" class="bottom-row__date" placeholder="MM/YY">
-                        <input type="text" class="bottom-row__cvv" placeholder="CVV">
+                        <input type="text" class="bottom-row__date" required="required" placeholder="MM/YY">
+                        <input type="number" class="bottom-row__cvv" required="required" placeholder="CVV">
                       </div>
                     </div>
                   </div>
@@ -75,4 +78,158 @@ export class PaymentPageView extends View<CartDataType> {
           ${this.views.footer.render()}
         `;
     }
+
+       public afterRender(controller: Controller): void {
+    super.afterRender(controller);
+
+    // валидация name
+    const nameInput  = document.querySelector(".payment-details__name") as HTMLInputElement;
+    nameInput.addEventListener('input', (event: Event) => {
+
+      nameInput.value = nameInput.value.replace(/[^\a-z, а-я]/g, "");
+      let arrName = Array.from(nameInput.value.split(' '))
+      if (arrName.length >= 2 && arrName.every((el) => el.length >= 3 )){
+        nameInput.classList.add('valid');
+        nameInput.classList.remove('invalid');
+      } else {
+        nameInput.classList.remove('valid');
+        nameInput.classList.add('invalid');
+      }
+    })
+
+    // валидация adress
+    const addressInput = document.querySelector(".payment-details__shipping-address") as HTMLInputElement;
+    addressInput.addEventListener('input', (event: Event) => {
+
+      addressInput.value = addressInput.value.replace(/[^\a-z, а-я]/g, "");
+      let arrAddress = Array.from(addressInput.value.split(' '))
+      if (arrAddress.length >= 3 && arrAddress.every((el) => el.length >= 5)){
+        addressInput.classList.add('valid');
+        addressInput.classList.remove('invalid');
+      } else {
+        addressInput.classList.remove('valid');
+        addressInput.classList.add('invalid');
+      }
+    })
+
+    // валидация email
+    const emailInput  = document.querySelector(".payment-details__email") as HTMLInputElement;
+    emailInput.addEventListener('input', (event: Event) => {
+      emailInput.classList.add('invalid');
+    })
+
+    // валидация phone-number
+    const numberInput  = document.querySelector(".payment-details__phone-number") as HTMLInputElement;
+    numberInput.addEventListener('input', (event: Event) => {
+
+      numberInput.value = numberInput.value.replace(/[^\0-9]/g, "").substring(0,16);
+      if (numberInput.value.match(/^\+\d{9}/g)){
+        numberInput.classList.add('valid');
+        numberInput.classList.remove('invalid');
+      } else {
+        numberInput.classList.remove('valid');
+        numberInput.classList.add('invalid');
+      }
+    })
+
+    // валидация card number
+    const cardNumberInput  = document.querySelector(".card-details__card-number") as HTMLInputElement;
+    const cardsImg  = document.querySelector(".cards__img") as HTMLElement;
+
+    cardNumberInput.addEventListener('input', (event: Event) => {
+      let arrCardNumber = Array.from(cardNumberInput.value)
+
+      let value: string | any = cardNumberInput.value.replace(/[^\d]/g, '').substring(0,16);
+      if (value != '') {
+        value = value.match(/.{1,4}/g).join(' ');
+      } else value = ''
+      cardNumberInput.value = value;
+
+      if (arrCardNumber[0] === '4') {
+        cardsImg.classList.add("cards__img_visa")
+      } else {
+        cardsImg.classList.remove("cards__img_visa")
+      }
+
+      if (arrCardNumber[0] === '5') {
+        cardsImg.classList.add("cards__img_mastercard")
+      } else {
+        cardsImg.classList.remove("cards__img_mastercard")
+      }
+
+      if (arrCardNumber[0] === '6') {
+        cardsImg.classList.add("cards__img_amex")
+      } else {
+        cardsImg.classList.remove("cards__img_amex")
+      }
+
+      if (arrCardNumber.length >= 19){
+        cardNumberInput.classList.add('valid');
+        cardNumberInput.classList.remove('invalid');
+      } else {
+        cardNumberInput.classList.remove('valid');
+        cardNumberInput.classList.add('invalid');
+      }
+    })
+
+    // валидация name on card
+    const nameCardInput  = document.querySelector(".card-details__name") as HTMLInputElement;
+    nameCardInput.addEventListener('input', (event: Event) => {
+
+      nameCardInput.value = nameCardInput.value.replace(/[^\a-z, а-я]/g, "");
+      let arrName = Array.from(nameCardInput.value.split(' '))
+
+      if (arrName.length >= 2 && arrName.every((el) => el.length >= 3 )){
+        nameCardInput.classList.add('valid');
+        nameCardInput.classList.remove('invalid');
+      }
+      else { 
+        nameCardInput.classList.remove('valid');
+        nameCardInput.classList.add('invalid');
+      }
+    })
+
+    // валидация CVV
+    const cvvInput  = document.querySelector(".bottom-row__cvv") as HTMLInputElement;
+    cvvInput.addEventListener('input', (event: Event) => {
+
+      cvvInput.value = cvvInput.value.replace(/[^\0-9]/g, "").substring(0,3);
+
+      if (cvvInput.value.match(/^\d{3}/g)){
+        cvvInput.classList.add('valid');
+        cvvInput.classList.remove('invalid')
+      }
+      else {
+        cvvInput.classList.remove('valid');
+        cvvInput.classList.add('invalid')
+      };
+    })
+
+    // валидация MM/YY
+    const dateInput  = document.querySelector(".bottom-row__date") as HTMLInputElement;
+    dateInput.addEventListener('input', (event: Event) => {
+
+      dateInput.value = dateInput.value.replace(/[^\0-9]/g, "").substring(0,5);
+
+      let value: string | any = dateInput.value.replace(/[^\d]/g, '').substring(0,4);
+      if (value != '') {
+        value = value.match(/.{1,2}/g).join('/');
+      } else value = ''
+      dateInput.value = value;
+
+      if (Number(dateInput.value.substring(0,2)) >= 12) {
+
+        dateInput.classList.add('invalid');
+      } else dateInput.classList.remove('invalid');
+
+      if (dateInput.value.match(/.{5}/g) && Number(dateInput.value.substring(0,2)) <= 12){
+        dateInput.classList.add('valid');
+        dateInput.classList.remove('invalid')
+      }
+      else {
+        dateInput.classList.remove('valid')
+        dateInput.classList.add('invalid');
+      }
+    })
+  } 
 }
